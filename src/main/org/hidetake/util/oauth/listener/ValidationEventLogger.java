a816112ad297fd00307ddffe2785064a047185cb
int128 <int128@gmail.com>
2010-01-28 16:34:00 +0000

    初期化時に設定ファイルの内容を表示するようにした。
    git-svn-id: http://opensocial-oauth-filter.googlecode.com/svn/trunk@49 6c3eec16-02c4-11df-bca2-d32778fd5087

--- src/main/org/hidetake/util/oauth/listener/ValidationEventLogger.java
+++ src/main/org/hidetake/util/oauth/listener/ValidationEventLogger.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.oauth.OAuthException;
+import net.oauth.signature.RSA_SHA1;
 
 import org.hidetake.util.oauth.ValidationEventListener;
 import org.hidetake.util.oauth.config.InitContext;
+import org.hidetake.util.oauth.model.OpenSocialAccessor;
 import org.hidetake.util.oauth.model.OpenSocialException;
 
 /**
@@ -27,6 +29,13 @@ public class ValidationEventLogger implements ValidationEventListener
 
 	public void init(InitContext context) throws ServletException
 	{
+		OpenSocialAccessor accessor = context.getOpenSocialAccessor();
+		log.info("App: " + accessor.getAppId() + ": " + accessor.getAppUrl());
+		log.info("Container: "
+			+ RSA_SHA1.X509_CERTIFICATE
+			+ ": "
+			+ accessor.getOAuthAccessor().consumer.getProperty(RSA_SHA1.X509_CERTIFICATE));
+		
 		for(ValidationEventListener listener : context.getValidationEventListeners()) {
 			log.info("Registered event listener: " + listener.getClass().getName());
 		}
@@ -40,7 +49,7 @@ public class ValidationEventLogger implements ValidationEventListener
 	public boolean onOAuthException(HttpServletRequest request, HttpServletResponse response, OAuthException e)
 	{
 		log.severe("Validation failed: " + e.getLocalizedMessage());
-		e.printStackTrace();
+		//e.printStackTrace();
 		return false;
 	}
 
