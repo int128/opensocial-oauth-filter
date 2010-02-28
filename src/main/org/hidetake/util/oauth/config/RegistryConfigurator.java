fd18a4eced3b2e31403088d5edd77a75619b86f4
int128 <int128@gmail.com>
2010-02-28 09:20:13 +0000

    add comment
    git-svn-id: http://opensocial-oauth-filter.googlecode.com/svn/trunk@115 6c3eec16-02c4-11df-bca2-d32778fd5087

--- src/main/org/hidetake/util/oauth/config/RegistryConfigurator.java
+++ src/main/org/hidetake/util/oauth/config/RegistryConfigurator.java
@@ -4,11 +4,27 @@ import org.hidetake.util.oauth.model.AppRegistry;
 import org.hidetake.util.oauth.model.ExtensionRegistry;
 
 
+/**
+ * Applying configuration to registries.
+ * 
+ * @author hidetake.org
+ *
+ */
 public interface RegistryConfigurator
 {
 
+	/**
+	 * Apply configuration to application registry.
+	 * @param registry
+	 * @throws ConfigurationException
+	 */
 	public void configure(AppRegistry registry) throws ConfigurationException;
 
+	/**
+	 * Apply configuration to extension registry.
+	 * @param registry
+	 * @throws ConfigurationException
+	 */
 	public void configure(ExtensionRegistry registry) throws ConfigurationException;
 
 }
